daba791ec5da6af4092a6485d444978be9e3c71d
timothé <timothe.laude@gmail.com>
2019-06-11 15:46:21 +0200

    jdbc auth

--- src/main/java/co/simplon/sec/SecurityConfig.java
+++ src/main/java/co/simplon/sec/SecurityConfig.java
@@ -1,28 +1,35 @@
 package co.simplon.sec;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder ;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+	@Autowired
+	private DataSource datasource; 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		// TODO Auto-generated method stub
-		auth.inMemoryAuthentication().withUser("user").password("{noop}tortue").roles("USER");
-		auth.inMemoryAuthentication().withUser("mohamed").password("{noop}elbabili").roles("ADMIN","USER");
+		/*auth.inMemoryAuthentication().withUser("user").password("{noop}tortue").roles("USER");
+		auth.inMemoryAuthentication().withUser("mohamed").password("{noop}elbabili").roles("ADMIN","USER");*/
+		auth.jdbcAuthentication()
+		.dataSource(datasource)
+		.usersByUsernameQuery("select username as principal,passwordname as credentials,active from T_Users where username=?")
+		.authoritiesByUsernameQuery("select username as principal,role as role from users_roles where username=?")
+		.rolePrefix("ROLE_")
+		.passwordEncoder(new BCryptPasswordEncoder());
 	}
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -31,6 +38,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		http.authorizeRequests().antMatchers("/operations","/consulterCompte").hasRole("USER");
 		http.authorizeRequests().antMatchers("/saveOperation").hasRole("ADMIN");
 		http.exceptionHandling().accessDeniedPage("/403");
+	}
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		   PasswordEncoder encoder = new BCryptPasswordEncoder();
+		    return encoder;
 	}
 	
 
---
+++ src/main/java/co/simplon/web/WebInitializer.java
@@ -0,0 +1,15 @@
+package co.simplon.web;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import co.simplon.MaBanqueEnLigneApplication;
+
+public class WebInitializer extends SpringBootServletInitializer{
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return builder.sources(MaBanqueEnLigneApplication.class);
+	}
+
+}
